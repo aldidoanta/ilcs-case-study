79b84bec6822101eff0ad085eb23e34871327827
Aldi Doanta Kurnia <aldidkurnia@gmail.com>
2025-08-07 21:25:37 +0700

    feat: finalize soution for kasus 1; add docs

--- 1/pipeline.py
+++ 1/pipeline.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sqlite3 import connect
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.lib import colors
 
 
 def extract_data(source_path):
@@ -33,6 +36,7 @@ def transform_data(df):
         crane_efficieny_data['efficiency_hour_per_weight'] = df['operation_time'] / \
             df['cargo_weight']
 
+        print("Data transformation successfully performed")
         return load_unload_data, crane_efficieny_data
     return None, None
 
@@ -47,13 +51,72 @@ def load_data(load_unload_data, crane_efficieny_data):
         print("load_unload_data is empty")
 
     if crane_efficieny_data is not None:
-        crane_efficieny_data.to_sql(name='load_unload', con=conn)
+        crane_efficieny_data.to_sql(name='crane_efficiency', con=conn)
         print("crane_efficieny_data loaded successfully to database")
     else:
         print("crane_efficieny_data is empty")
+    return conn
 
-# def generate_report():
-#     """Loads"""
+
+def generate_report(db_connection):
+    """Generates a PDF containing the aggregated data from database"""
+
+    # query load_unload data
+    # sort by date and duration (descending)
+    load_unload_report = pd.read_sql(
+        '''SELECT date,ship_id,duration_hour
+            FROM load_unload
+            WHERE date > (SELECT DATETIME('now', '-7 day'))
+            ORDER BY date ASC, duration_hour DESC
+        ''', db_connection)
+
+    # query crane_efficiency data
+    # sort by date and efficiency (ascending)
+    crane_efficiency_report = pd.read_sql(
+        '''SELECT date,crane_id,efficiency_hour_per_weight
+            FROM crane_efficiency
+            WHERE date > (SELECT DATETIME('now', '-7 day'))
+            ORDER BY date ASC, efficiency_hour_per_weight ASC
+        ''', db_connection)
+
+    # generate the pdf
+    pdf = SimpleDocTemplate("report.pdf", pagesize=letter)
+
+    table_style = TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('FONTSIZE', (0, 0), (-1, 0), 14),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+        ('TEXTCOLOR', (0, 1), (-1, -1), colors.black),
+        ('ALIGN', (0, 1), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 1), (-1, -1), 'Helvetica'),
+        ('FONTSIZE', (0, 1), (-1, -1), 12),
+        ('BOTTOMPADDING', (0, 1), (-1, -1), 8),
+    ])
+
+    table_data = []
+    pdf_table = []
+    table_data.append(list(load_unload_report.columns))
+    for _, row in load_unload_report.iterrows():
+        table_data.append(list(row))
+    table = Table(table_data)
+    table.setStyle(table_style)
+    pdf_table.append(table)
+
+    table2_data = []
+    table2_data.append(list(crane_efficiency_report.columns))
+    for _, row in crane_efficiency_report.iterrows():
+        table2_data.append(list(row))
+    table2 = Table(table2_data)
+    table2.setStyle(table_style)
+    pdf_table.append(table2)
+
+    pdf.build(pdf_table)
+    print("Report PDF successfully created")
+
 
 def main():
     source_file = './input.csv'
@@ -61,8 +124,9 @@ def main():
 
     raw_data = extract_data(source_file)
     load_unload_data, crane_efficieny_data = transform_data(raw_data)
-    load_data(load_unload_data, crane_efficieny_data)
-    # generate_report()
+    db_connection = load_data(load_unload_data, crane_efficieny_data)
+    generate_report(db_connection)
+
 
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # ilcs-case-study
+
+## Overview
+- Each case study is put into its respective folder (/1, /2, /3)
+
+## Case 1
+### Requirements/Preparations
+1. Python 3 with venv
+
+### Installation
+1. Create and activate the virtual environment.
+```bash
+python3 -m venv env
+source env/bin/activate
+```
+
+2. Install app dependencies.
+```bash
+pip install -r requirements.txt
+```
+
+3. cd into the `/1` folder 
+```bash
+cd 1
+```
+
+4. Run the Python script
+```bash
+python3 pipeline.py
+```
+
+5. To stop and deactivate the virtual environment, <kbd>CTRL</kbd>+<kbd>C</kbd> then:
+```bash
+deactivate
+```
